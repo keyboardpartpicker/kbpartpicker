1463595732ea4f125b91754859df6015adff9331
EthanChin <echin425@gmail.com>
2025-07-06 14:24:06 -0700

    Updated backend API to use PostgreSQL for the database

--- api/server.js
+++ api/server.js
@@ -5,12 +5,13 @@ const app = express();
 
 app.use(cors());
 const pool = new Pool({
-    user: 'kbadmin',
-    host: 'localhost',
-    database: 'kbdb',
-    password: 'securepass',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10),
 });
+
 
 app.get('/switches', async (req, res) => {
     const result = await pool.query('SELECT * FROM switches');
