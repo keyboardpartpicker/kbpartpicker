d7482027c56fdabf827cf87af6f52fb739bc2d9d
EthanChin <echin425@gmail.com>
2025-09-26 11:54:12 -0700

    Fix to kb database API.

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 
 const pool = new Pool({
   user: process.env.DB_USER,
